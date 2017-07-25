79ed3804e518c900a6786af1832bac4c09b7d313
LCZ <mystric@foxmail.com>
2017-07-25 10:04:20 +0800

    bing search page

---
+++ 11.js
@@ -0,0 +1,57 @@
+{
+    "AS"
+:
+    {
+        "Query"
+    :
+        "3", "FullResults"
+    :
+        1, "Results"
+    :
+        [{
+            "Type": "AS",
+            "Suggests": [{
+                "Txt": "360",
+                "Type": "AS",
+                "Sk": "",
+                "Url": "http://www.bing.com/results.aspx?q=360\u0026qs=AS\u0026sk=",
+                "HCS": 1
+            }, {
+                "Txt": "360安全卫士下载",
+                "Type": "AS",
+                "Sk": "AS1",
+                "Url": "http://www.bing.com/results.aspx?q=360%e5%ae%89%e5%85%a8%e5%8d%ab%e5%a3%ab%e4%b8%8b%e8%bd%bd\u0026qs=AS\u0026sk=AS1"
+            }, {
+                "Txt": "360浏览器",
+                "Type": "AS",
+                "Sk": "AS2",
+                "Url": "http://www.bing.com/results.aspx?q=360%e6%b5%8f%e8%a7%88%e5%99%a8\u0026qs=AS\u0026sk=AS2"
+            }, {
+                "Txt": "360安全浏览器",
+                "Type": "AS",
+                "Sk": "AS3",
+                "Url": "http://www.bing.com/results.aspx?q=360%e5%ae%89%e5%85%a8%e6%b5%8f%e8%a7%88%e5%99%a8\u0026qs=AS\u0026sk=AS3"
+            }, {
+                "Txt": "360安全卫士",
+                "Type": "AS",
+                "Sk": "AS4",
+                "Url": "http://www.bing.com/results.aspx?q=360%e5%ae%89%e5%85%a8%e5%8d%ab%e5%a3%ab\u0026qs=AS\u0026sk=AS4"
+            }, {
+                "Txt": "360云盘",
+                "Type": "AS",
+                "Sk": "AS5",
+                "Url": "http://www.bing.com/results.aspx?q=360%e4%ba%91%e7%9b%98\u0026qs=AS\u0026sk=AS5"
+            }, {
+                "Txt": "360软件管家",
+                "Type": "AS",
+                "Sk": "AS6",
+                "Url": "http://www.bing.com/results.aspx?q=360%e8%bd%af%e4%bb%b6%e7%ae%a1%e5%ae%b6\u0026qs=AS\u0026sk=AS6"
+            }, {
+                "Txt": "360极速浏览器",
+                "Type": "AS",
+                "Sk": "AS7",
+                "Url": "http://www.bing.com/results.aspx?q=360%e6%9e%81%e9%80%9f%e6%b5%8f%e8%a7%88%e5%99%a8\u0026qs=AS\u0026sk=AS7"
+            }]
+        }]
+    }
+}
